27c7358abb778007fe5e3b4909e68f4fc1ba3329
Arturo Ceron <43083944+ArturoCeron@users.noreply.github.com>
2020-06-15 12:38:03 -0700

    Update Practica2.s

--- Practica2.s
+++ Practica2.s
@@ -3,12 +3,12 @@ cadena : .asciz " Hola Mundo !\ n "
 cadenafin :
 .text
 .global main
-MAIN : push { r7, lr } /* preservamos reg. */
-MOV r0, # 1 /* salida est á ndar */
-LDR r1, = cadena /* cadena a enviar */
-MOV r2, # cadenafin - cadena /* longitud */
-MOV r7, # 4 /* seleccionamos la */
-SWI #0 /* llamada a sistema ’write ’*/
-MOV r0, # 0 /* devolvemos ok */
-POP { r7, lr } /* recuperamos reg. */
-BX lr /* salimos de main */
+main: push { r7, lr } /* preservamos reg. */
+mov r0, # 1 /* salida est á ndar */
+ldr r1, = cadena /* cadena a enviar */
+mov r2, # cadenafin - cadena /* longitud */
+mov r7, # 4 /* seleccionamos la */
+swi #0 /* llamada a sistema ’write ’*/
+mov r0, # 0 /* devolvemos ok */
+pop { r7, lr } /* recuperamos reg. */
+bx lr /* salimos de main */
