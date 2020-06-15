005cf02c6a4a56bdea16bbc3216e2222b13a433f
Arturo Ceron <43083944+ArturoCeron@users.noreply.github.com>
2020-06-15 11:31:11 -0700

    Add files via upload

---
+++ Practica2.s
@@ -0,0 +1,14 @@
+.data
+cadena : .asciz " Hola Mundo !\ n "
+cadenafin :
+.text
+.global main
+MAIN : push { r7, lr } /* preservamos reg. */
+MOV r0, # 1 /* salida est á ndar */
+LDR r1, = cadena /* cadena a enviar */
+MOV r2, # cadenafin - cadena /* longitud */
+MOV r7, # 4 /* seleccionamos la */
+SWI #0 /* llamada a sistema ’write ’*/
+MOV r0, # 0 /* devolvemos ok */
+POP { r7, lr } /* recuperamos reg. */
+BX lr /* salimos de main */
